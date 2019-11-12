fa93664e4b34863b557750c1ab2a7797803cf520
alycia hall <amhall25@gmail.com>
2019-11-12 02:15:56 +0000

    update environment

--- lib/Enviornment.rb
+++ lib/Enviornment.rb
@@ -1,4 +1,11 @@
-require "Waves/version"
+require_relative "Waves/version"
+require_relative "Waves/cli"
+require_relative "Waves/scrapper"
+require_relative "Waves/beaches"
+
+require "pry"
+require "nokogiri"
+require "open-uri"
 
 module Waves
   class Error < StandardError; end
---
+++ lib/Waves/cli.rb
@@ -0,0 +1,7 @@
+class Waves::CLI 
+  def call
+    puts "welcome to Waves, enter the zip code you wish to know more about"
+    
+    input = gets.chomp
+  end
+end
